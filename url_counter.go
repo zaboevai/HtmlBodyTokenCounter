356cff421c4840b956b64ae02ebcc2140b7ae923
Zaboev Andrey <zaboevai@yandex.ru>
2020-05-24 14:40:38 +0300

    init token counter from html body

---
+++ url_counter.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type Result struct {
+	url        string
+	token      string
+	tokenCount int
+}
+
+func main() {
+	const (
+		goRoutineLimit = 5
+		token          = "Go"
+	)
+
+	var (
+		inputChanel  = make(chan string)
+		urlsChanel   = make(chan string, goRoutineLimit-1)
+		resultChanel = make(chan Result)
+	)
+
+	go ParseStdin(inputChanel)
+	go ValidateUrls(inputChanel, urlsChanel)
+	go CountTokenFromUrls(token, urlsChanel, resultChanel)
+	PrintResult(resultChanel)
+
+}
+
+func ParseStdin(inputChanel chan string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		inputString := scanner.Text()
+		inputChanel <- inputString
+	}
+	close(inputChanel)
+}
+
+func ValidateUrls(inputChanel chan string, urlsChanel chan string) {
+	for input := range inputChanel {
+		url_, err := url.ParseRequestURI(input)
+		if err != nil {
+			fmt.Println("Строка=", input, ", ошибка=", err)
+			continue
+		}
+		urlsChanel <- url_.String()
+	}
+	close(urlsChanel)
+}
+
+func CountTokenFromUrls(token string, urlsChanel chan string, resultChanel chan Result) {
+	var (
+		isDoneChanel = make(chan bool)
+		workers      = 0
+	)
+
+	for url_ := range urlsChanel {
+		workers++
+		go countTokenFromUrl(url_, token, resultChanel, isDoneChanel)
+	}
+
+	// Дожидаюсь завершения всех заданий
+	for done := range isDoneChanel {
+		if !done {
+			fmt.Println("worker error")
+		}
+		workers--
+		if workers == 0 {
+			break
+		}
+	}
+	close(isDoneChanel)
+	close(resultChanel)
+}
+
+func PrintResult(resultChanel chan Result) {
+	sum := 0
+	for result := range resultChanel {
+		fmt.Println("Count for", result.url, ":", result.tokenCount)
+		sum += result.tokenCount
+	}
+	fmt.Println("Total:", sum)
+}
+
+func countTokenFromUrl(url string, token string, resultChanel chan Result, done chan bool) {
+	responseBody := sendRequest(url)
+	if responseBody == "" {
+		resultChanel <- Result{url, token, 0}
+	}
+	count := strings.Count(responseBody, token)
+	resultChanel <- Result{url, token, count}
+	done <- true
+}
+
+func sendRequest(url string) string {
+	response, err := http.Get(url)
+	if response != nil {
+		if err != nil {
+			fmt.Println("Ошибка.", response.Status, "name=", url)
+			return ""
+		}
+
+		body, err := ioutil.ReadAll(response.Body)
+		if err == nil {
+			return string(body)
+		}
+	}
+	return ""
+}
